6a0595121eff35290d108509e927f0ca7f214021
Shibra Amin <shibraamin18@gmail.com>
2021-05-01 02:53:02 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,8 +2,8 @@
 #!/bin/bash
 cd /home/ec2-user/server
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt-get install nodejs
+sudo apt-get install nodejs -y
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
-sudo apt-get install -y mongodb-org
-sudo systemctl start mongod
+sudo apt-get install mongodb-org -y
+sudo systemctl start mongod -y
 
