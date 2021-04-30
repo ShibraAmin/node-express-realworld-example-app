3b144b3ccbf9727f44e7c82163cff61a1beabfd9
Shibra Amin <shibraamin18@gmail.com>
2021-05-01 02:52:00 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -6,4 +6,4 @@ sudo apt-get install nodejs
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 sudo apt-get install -y mongodb-org
 sudo systemctl start mongod
-mongo
+
