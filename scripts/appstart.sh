49a06c26039d28de3c464c614837206aed6a56dd
Shibra Amin <shibraamin18@gmail.com>
2021-05-12 15:12:12 +0530

    Update appstart.sh

--- scripts/appstart.sh
+++ scripts/appstart.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/node-express-realworld-example-app
-NODE_ENV=production pm2 start app.js
+sudo ln -s /ubuntu/.npm-global/lib/node_modules/pm2/bin/pm2 /usr/bin/pm2
+pm2 start app.js
